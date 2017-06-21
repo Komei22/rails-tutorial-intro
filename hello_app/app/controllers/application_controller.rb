15a607c904f14d2c9001029220fd7dceeea4c2b8
Komei Nomura <komei.nomura@pepabo.com>
2017-06-21 15:32:59 +0900

    'hola'アクションと'goodbye'アクション追加

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "hello, world!"
   end
+
+  def hola
+    render html: "¡hola, mundo!"
+  end
+
+  def goodbye
+    render html: "goodbye, world!"
+  end
 end
